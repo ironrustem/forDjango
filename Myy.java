4d5dd3fd1d97a427ab9d36b0687406bd7aacf6c5
Saifullin Rustem <ironrustem@gmail.com>
2020-05-20 16:45:01 +0300

    20.05.2020

---
+++ Myy.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Phyton для меня был всегда интересен, в 11 классе начал его изучать, но затем пришлось отдать приоритет на программирование на pascal для ЕГЭ." +
+        	"Думаю мне будет очень интересно работать с django и phyton. Спасибо.");
+    }
+}
+
